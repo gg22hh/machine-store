84d53dd6dfc0b4e3cf18be049d36dc2fd6aa5710
g322h <play02s@mail.ru>
2021-06-09 23:52:53 +0300

    header markup

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,23 @@ export const Header: FC = () => {
   return (
     <header className="header">
       <div className="container">
-        <img src="/images/logo.png" alt="" />
+        <div className="header__inner">
+          <img className="header__logo" src="/images/logo.png" alt="" />
+          <nav className="header__menu">
+            <ul className="header__menu-list">
+              <li className="active"><a href="#">Home</a></li>
+              <li><a href="#">Products</a></li>
+              <li><a href="#">Artists</a></li>
+              <li><a href="#">Contact us</a></li>
+              <li><a href="#">About</a></li>
+            </ul>
+          </nav>
+          <div className="header__cart">
+            <div className="header__cart-number">1</div>
+            <a href="#"><img src="/images/card.svg" alt="" /></a>
+          </div>
+        </div>
+
       </div>
     </header>
   );
