9967f7c6aa23e6fb0d61c1ecda3ba29c32a90f19
And <79831859+and-bulk@users.noreply.github.com>
2021-06-10 01:46:32 +0400

    fix tag & class names

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,21 +6,22 @@ export const Header: FC = () => {
       <div className="container">
         <div className="header__inner">
           <img className="header__logo" src="/images/logo.png" alt="" />
-          <nav className="header__menu">
-            <ul className="header__menu-list">
-              <li className="active"><a href="#">Home</a></li>
-              <li><a href="#">Products</a></li>
-              <li><a href="#">Artists</a></li>
-              <li><a href="#">Contact us</a></li>
-              <li><a href="#">About</a></li>
+
+          <nav className="nav">
+            <ul className="nav__list">
+              <li className="nav__item"><a className="nav__link" href="#">Home</a></li>
+              <li className="nav__item"><a className="nav__link" href="#">Products</a></li>
+              <li className="nav__item"><a className="nav__link" href="#">Artists</a></li>
+              <li className="nav__item"><a className="nav__link" href="#">Contact us</a></li>
+              <li className="nav__item"><a className="nav__link" href="#">About</a></li>
             </ul>
           </nav>
-          <div className="header__cart">
-            <div className="header__cart-number">1</div>
-            <a href="#"><img src="/images/card.svg" alt="" /></a>
-          </div>
-        </div>
 
+          <a href="#" className="cart">
+            <span className="cart__count">1</span>
+            <img className="cart__image" src="/images/card.svg" alt="" />
+          </a>
+        </div>
       </div>
     </header>
   );
